8fe1fc9928c9360944bee7f40db69372c376c786
barrtp <123276307+barrtp@users.noreply.github.com>
2023-03-24 19:59:09 -0400

    updated file locations

---
+++ barrt_pgm5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  barrt_pgm5
+//
+//  Created by Theodore Barr on 3/24/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    // this is where i will eventually make program 5 :)
+    std::cout << "Hello, World!\n";
+    return 0;
+}
